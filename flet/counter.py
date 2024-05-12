8b804434e47a8a5c1c3090163d0b7b6b804169e6
Jguerrero45 <guerrerojosegarcia@gmail.com>
2024-05-12 10:25:07 -0400

    Agregado los archivos de flet a su carpeta

---
+++ flet/counter.py
@@ -0,0 +1,33 @@
+import flet
+from flet import Page, TextField, Row, IconButton, icons
+
+
+def main(page: Page):
+    page.title = "Contador"
+    page.vertical_alignment = "center"
+    page.horizontal_alignment = "center"
+
+    txt_numer = TextField(value='0', text_align='right', width=100)
+
+    def minus_click(event):
+        if txt_numer.value > 0:
+            txt_numer.value = int(txt_numer.value) - 1
+            page.update()
+        else:
+            pass
+
+    def plus_click(event):
+        txt_numer.value = int(txt_numer.value) + 1
+        page.update()
+
+    page.add(
+        Row([
+            IconButton(icons.REMOVE, on_click=minus_click),
+            txt_numer,
+            IconButton(icons.ADD, on_click=plus_click)
+        ])
+    )
+
+
+# flet.app(target=main)
+flet.app(target=main, view=flet.WEB_BROWSER)
