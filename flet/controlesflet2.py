e315cfad37a792762da0ac5885688e10def52b93
Jguerrero45 <guerrerojosegarcia@gmail.com>
2024-05-14 02:19:19 -0400

    Actualizacion de controles con update() donde hacemos un contador utilizando time

---
+++ flet/controlesflet2.py
@@ -0,0 +1,16 @@
+import flet as ft
+import time
+from flet import Page, Text
+
+
+def main(page: Page):
+    t = Text()
+    page.add(t)
+
+    for i in range(-10):
+        t.value = f'step: {i}'
+        page.update()
+        time.sleep(1)
+
+
+ft.app(target=main, view=ft.WEB_BROWSER)
